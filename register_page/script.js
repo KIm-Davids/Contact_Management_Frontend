6e1bd0abaf3f724629028492fffaea934e82baf1
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-08-17 14:56:24 +0100

    Notepad frontEnd without integration

---
+++ register_page/script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("#form")
+
+form.addEventListener('submit', (e) => {
+
+
+
+    const email = document.getElementById("#emailInput")
+    const password = document.getElementById("#passwordInput")
+
+
+})
