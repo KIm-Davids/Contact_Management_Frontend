4db905a93adfcf3eb8abd43b50db4b98e227772c
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-08-18 09:18:42 +0100

    Updated Notepad front end

--- register_page/script.js
+++ register_page/script.js
@@ -1,11 +1,33 @@
 const form = document.querySelector("#form")
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
+    const email = document.getElementById("emailInput")
+    const password = document.getElementById("passwordInput")
 
-
-    const email = document.getElementById("#emailInput")
-    const password = document.getElementById("#passwordInput")
-
+    try{
+        console.log("one")
+        const response  = await fetch("http://localhost:8089/login/notepad/login-user",
+            {
+                method: "PATCH",
+                headers: {
+                    "content-type" : "application/json"
+                },
+                body:JSON.stringify({email:email, password:password})
+            })
+        console.log("Two")
+        if(response.ok){
+            const result = await response.text();
+            alert('success')
+            console.log(result)
+        }else{
+            const err = await response.text();
+            alert(err)
+            console.log(err)
+        }
+    }catch(error){
+        alert(error.message)
+    }
 
 })
